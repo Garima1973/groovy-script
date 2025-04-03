837ff59e1a3af0e8cdfbcad85c470959d6ba0733
Garima1973 <garimam612@gmail.com>
2025-04-03 14:57:45 +0530

    pule code into deployment

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -9,5 +9,15 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/Garima1973/front-end-application.git'
             }
         }
+
+        stage('deploy'){
+
+            steps{
+                sshagent(['frontend']) {
+                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.234.66.239 "sudo rm -r /var/www/html/*"'
+                    sh 'scp -o StrictHostKeyChecking=no -r * ubuntu@13.234.66.239:/home/ubuntu/frontend/'
+                    }
+            }
+        }
     }
 }
