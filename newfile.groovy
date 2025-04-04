b01c48037a0cac5e7d83240dcb59e9dd86f4ede5
Garima1973 <garimam612@gmail.com>
2025-04-04 14:29:48 +0530

    restart backend

---
+++ newfile.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+        tools{
+            maven 'maven-3.9.8'
+        }
+    stages {
+
+        stage('pull code'){
+
+            steps{
+                git branch: 'main', url: 'https://github.com/Garima1973/For-Backend-Developer.git'
+            }
+        }
+
+        stage('build'){
+
+            steps{
+                sh 'mvn clean package'
+            }
+        }
+
+        //  stage('Restart Nginx'){
+
+        //     steps{
+        //         sshagent(['frontend']) {
+        //             sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.234.66.239 "sudo systemctl restart nginx"'
+
+        //             }
+        //     }
+        // }
+    }
+}
