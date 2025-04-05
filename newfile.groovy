c9fe9e78908c35ee7735cd3755bc83b6bc0dbc88
Garima1973 <garimam612@gmail.com>
2025-04-05 14:20:36 +0530

    restart backend new clent webapp

--- newfile.groovy
+++ newfile.groovy
@@ -9,7 +9,7 @@ pipeline {
         stage('pull code'){
 
             steps{
-                git branch: 'main', url: 'https://github.com/Garima1973/Clientapp.git'
+                git branch: 'main', url: 'https://github.com/Garima1973/newclientapp.git'
             }
         }
 
