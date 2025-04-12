36a4916df90567c646ff08367c5a472fa8d7d340
Garima1973 <garimam612@gmail.com>
2025-04-12 15:54:34 +0530

    Deploy command now fast

--- newfile.groovy
+++ newfile.groovy
@@ -9,7 +9,7 @@ pipeline {
         stage('pull code'){
 
             steps{
-                git branch: 'main', url: 'https://github.com/Garima1973/forbackend-.git'
+                git branch: 'main', url: 'https://github.com/Garima1973/forbackentLast.git'
             }
         }
 
