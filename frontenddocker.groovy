3368f1c2d34552f9f434729149248be6f4a43ef4
Garima1973 <garimam612@gmail.com>
2025-05-27 16:15:59 +1000

    second commit

--- frontenddocker.groovy
+++ frontenddocker.groovy
@@ -6,7 +6,7 @@ pipeline {
         stage('pull code'){
 
             steps{
-                git branch: 'main', url: 'https://github.com/Garima1973/front-end-application.git'
+                git branch: 'main', url: 'https://github.com/Garima1973/groovy-script.git'
             }
         }
 
