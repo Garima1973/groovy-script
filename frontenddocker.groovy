c712e557b8be33208cbb05e84fe7e88f4446ba03
Garima1973 <garimam612@gmail.com>
2025-05-29 16:11:15 +1000

    9 commit

--- frontenddocker.groovy
+++ frontenddocker.groovy
@@ -24,7 +24,7 @@ pipeline {
             steps{
                 sshagent(['docker_server'])
                 {
-                     sh 'ssh -o StrictHostKeyChecking=no ubuntu@3.24.136.56 "docker build -t frontenddocker_jenkins"'
+                     sh 'ssh -o StrictHostKeyChecking=no ubuntu@3.24.136.56 "docker build -t frontenddocker_jenkins ."'
                 }
             }
         }
