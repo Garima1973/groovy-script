bcee8c282207785dc98ccf8b856aa5e9c51bbb1c
Garima1973 <garimam612@gmail.com>
2025-06-23 16:21:17 +1000

    new commit

---
+++ backenddocker.groovy
@@ -0,0 +1,50 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('pull code'){
+
+            steps{
+                git branch: 'main', url: 'https://github.com/Garima1973/For-Backend-Docker.git'
+            }
+        }
+        stage('sending files to docker server')
+        {
+            steps{
+                  sshagent(['docker_server'])
+                   {
+                sh 'scp -o StrictHostKeyChecking=no -r * ubuntu@13.211.158.126:/home/ubuntu/backendocker/'
+                // public ip of docker because docker is the destination
+                  }
+            }
+        }
+        // stage('docker image')
+        // {
+        //     steps{
+        //         sshagent(['docker_server'])
+        //         {
+        //              sh 'ssh -o StrictHostKeyChecking=no ubuntu@3.107.192.112 "docker build -t frontenddockerjenkins /home/ubuntu/frontenddocker"'
+        //         }
+        //     }
+        // }
+
+        // stage('docker run')
+        // {
+        //     steps{
+        //         sshagent(['docker_server'])
+        //         {   
+        //            sh 'ssh -o StrictHostKeyChecking=no ubuntu@3.107.192.112 "docker stop freedocker"' 
+
+        //            sh 'ssh -o StrictHostKeyChecking=no ubuntu@3.107.192.112 "docker rm freedocker"' 
+
+ 
+        //            sh 'ssh -o StrictHostKeyChecking=no ubuntu@3.107.192.112 "docker run -d -p 5007:80 --name freedocker frontenddockerjenkins"' 
+        //         }
+        //     }
+        // }
+
+          
+
+    }
+ }
